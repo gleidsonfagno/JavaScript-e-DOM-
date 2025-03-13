9751d43d39d605a3ecf0fc3b6b68e5c191e88df7
gleidson fagno <gleidsonfagner838@gmail.com>
2025-03-12 22:41:55 -0300

    feat: Validando os campos do formuario e adicionando um borda vermelha

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # JavaScript e DOM
 
-Criando uma calendário de aniversários com CRUD básico
+## Criando uma calendário de aniversários com CRUD básico
 
-## Desafio do dia 1 criar o formulario é uma fução que pegue os dados e mostrar no console.
+### Desafio do dia 1 criar o formulario é uma fução que pegue os dados e mostrar no console
+
+### Desafio do dia 2 validar os campos  dos inputs
+
+- Um nome precisa ter no mínimo três letras.
+- Um nome pode ter no máximo 120 letras.
+- Um nome pode apenas conter letras, e não números.
+- A data de nascimento precisa estar no formato DD/MM/AAAA, por exemplo: 31/01/2021.
+-O mês informado deve estar entre 01 e 12.
--- src/script.js
+++ src/script.js
@@ -1,13 +1,58 @@
-const form = document.querySelector(".js-form")
-const user_name = document.querySelector(".js-field-name")
-const date_of_birth = document.querySelector(".js-field-data")
-const button_submit = document.querySelector(".submit")
+const form = document.querySelector(".js-form");
+let user_name = document.querySelector(".js-field-name");
+const date_of_birth = document.querySelector(".js-field-data");
+const button_submit = document.querySelector(".submit");
 
+function validateUserName(name) {
+  const regex = /^[A-Za-zÀ-ÿ\s]+$/;
 
-button_submit.addEventListener("click", function(event) {
-    event.preventDefault()
+  if (name && name.trim() === "") {
+    alert("O campo está em branco");
+    return false;
+  }
 
-    console.log(user_name.value)
-    console.log(date_of_birth.value)
-    
-})
+  if (!regex.test(name)) {
+    alert("Somente letras é permitido");
+    return false;
+  }
+
+  return true;
+}
+
+function validateDateOfBirth(date) {
+  const [year, month, day] = date.split("-").map(Number);
+
+  newData = new Date(year, month - 1, day);
+
+  if (year.toString().length > 4) {
+    alert("O ano da data de nascimento não pode ter mais de 4 dígitos.");
+    return false;
+  } else {
+    return newData.toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+  }
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const nameValid = validateUserName(user_name.value);
+  const datelavid = validateDateOfBirth(date_of_birth.value);
+
+  if (!nameValid ) {
+    user_name.classList.add("erro");
+  }else{
+    date_of_birth.classList.remove("erro")
+  }
+
+  if(!datelavid){
+    date_of_birth.classList.add("erro")
+  }else{
+    date_of_birth.classList.remove("erro")
+  }
+  
+  
+});
